08f7120b46c152733477357e7957eff8ab351a7d
zoubinthw <316324334@qq.com>
2023-02-06 22:26:30 +0800

    [06]商品品牌图片管理

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -19,14 +19,16 @@
         <el-switch
           v-model="dataForm.showStatus"
           active-color="#13ce66"
-          inactive-color="#ff4949">
+          inactive-color="#ff4949"
+          :active-value="1"
+          :inactive-value="0">
         </el-switch>
       </el-form-item>
       <el-form-item label="检索首字母" prop="firstLetter">
         <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
       </el-form-item>
       <el-form-item label="排序" prop="sort">
-        <el-input v-model="dataForm.sort" placeholder="排序"></el-input>
+        <el-input v-model.number="dataForm.sort" placeholder="排序"></el-input>
       </el-form-item>
     </el-form>
     <span slot="footer" class="dialog-footer">
@@ -48,9 +50,9 @@ export default {
         name: '',
         logo: '',
         descript: '',
-        showStatus: '',
+        showStatus: 1,
         firstLetter: '',
-        sort: ''
+        sort: 0
       },
       dataRule: {
         name: [
@@ -66,10 +68,30 @@ export default {
           {required: true, message: '显示状态[0-不显示；1-显示]不能为空', trigger: 'blur'}
         ],
         firstLetter: [
-          {required: true, message: '检索首字母不能为空', trigger: 'blur'}
+          {validator: (rule, value, callback) => {
+            if (value === '') {
+              callback(new Error('首字母必须填写'))
+            } else if (!/^[a-zA-Z]$/.test(value)) {
+              callback(new Error('首字母必须是a-z或A-Z'))
+            } else {
+              callback()
+            }
+          },
+            trigger: 'blur'}
         ],
         sort: [
-          {required: true, message: '排序不能为空', trigger: 'blur'}
+          {
+            validator: (rule, value, callback) => {
+              if (value === '') {
+                callback(new Error('排序字段必须填写'))
+              } else if (!Number.isInteger(value) || value < 0) {
+                callback(new Error('排序字段必须是一个大于等于0的整数'))
+              } else {
+                callback()
+              }
+            },
+            trigger: 'blur'
+          }
         ]
       }
     }
