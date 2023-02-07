a17ab148e206a26bbde4dc78ddd7a143a53e4802
zb605 <18840818956@163.com>
2023-02-07 23:10:31 +0800

    [69]商品信息的编辑

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -161,7 +161,7 @@ export default {
       let {brandId, showStatus} = data
       // 发送请求，修改状态
       this.$http({
-        url: this.$http.adornUrl('/product/brand/update'),
+        url: this.$http.adornUrl('/product/brand/update/status'),
         method: 'post',
         data: this.$http.adornData({brandId, showStatus}, false)
       }).then(({data}) => {
